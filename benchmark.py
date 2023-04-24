16d8422b52243c05ecb484cf690242d097c7d150
Lianming Du <adullb@qq.com>
2023-04-24 18:06:15 +0800

    update ssr

--- benchmark.py
+++ benchmark.py
@@ -1,7 +1,7 @@
 import time
 import stria
 import pyfastx
-gfile = 'Homo_sapiens.GRCh38.dna.toplevel.fa.gz'
+gfile = 'chr1.fa.gz'
 
 for name, seq in pyfastx.Fastx(gfile):
 	break
--- src/ssr.c
+++ src/ssr.c
@@ -274,42 +274,15 @@ static PyMethodDef stria_ssrminer_methods[] = {
 };
 
 PyTypeObject stria_SSRMinerType = {
-    PyVarObject_HEAD_INIT(NULL, 0)
-    "SSRMiner",                        /* tp_name */
-    sizeof(stria_SSRMiner),          /* tp_basicsize */
-    0,                              /* tp_itemsize */
-    (destructor)stria_ssrminer_dealloc,   /* tp_dealloc */
-    0,                              /* tp_print */
-    0,                              /* tp_getattr */
-    0,                              /* tp_setattr */
-    0,                              /* tp_reserved */
-    (reprfunc)stria_ssrminer_repr,                              /* tp_repr */
-    0,                              /* tp_as_number */
-    0,                   /* tp_as_sequence */
-    0,                   /* tp_as_mapping */
-    0,                              /* tp_hash */
-    0,                              /* tp_call */
-    0,                              /* tp_str */
-    0,                              /* tp_getattro */
-    0,                              /* tp_setattro */
-    0,                              /* tp_as_buffer */
-    Py_TPFLAGS_DEFAULT,             /* tp_flags */
-    "find microsatellites from DNA sequence",                              /* tp_doc */
-    0,                              /* tp_traverse */
-    0,                              /* tp_clear */
-    0,                              /* tp_richcompare */
-    0,                              /* tp_weaklistoffset */
-    (getiterfunc)stria_ssrminer_iter,     /* tp_iter */
-    (iternextfunc)stria_ssrminer_next,    /* tp_iternext */
-    stria_ssrminer_methods,          /* tp_methods */
-    0,          /* tp_members */
-    0,                               /* tp_getset */
-    0,                              /* tp_base */
-    0,                              /* tp_dict */
-    0,                              /* tp_descr_get */
-    0,                              /* tp_descr_set */
-    0,                              /* tp_dictoffset */
-    0,                              /* tp_init */
-    0,            /* tp_alloc */
-    stria_ssrminer_new,              /* tp_new */
+	PyVarObject_HEAD_INIT(NULL, 0)
+	.tp_name = "SSRMiner",
+	.tp_basicsize = sizeof(stria_SSRMiner),
+	.tp_dealloc = (destructor)stria_ssrminer_dealloc,
+	.tp_repr = (reprfunc)stria_ssrminer_repr,
+	.tp_flags = Py_TPFLAGS_DEFAULT,
+	.tp_doc = "find microsatellites from DNA sequence",
+	.tp_iter = (getiterfunc)stria_ssrminer_iter,
+	.tp_iternext = (iternextfunc)stria_ssrminer_next,
+	.tp_methods = stria_ssrminer_methods,
+	.tp_new = stria_ssrminer_new,
 };
